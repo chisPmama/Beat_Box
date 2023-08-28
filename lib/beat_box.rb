cf38ef02373ccecdd870497f4145b0ba2edcc2f0
Anthea Yur <acyur6@gmail.com>
2023-08-28 18:52:42 -0500

    Change initialize to append the string argument and change append, works only for if one value and included

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -1,20 +1,42 @@
 require './lib/linked_list'
-# require 'pry'
+require 'pry'
 
 class BeatBox
+    attr_reader :list, :data
 
-    attr_reader :list
-
-    def initialize
-        @list = LinkedList.new #Created a new LinkedList that is stored in BeatBox
+# ITERATION V
+    def initialize(data)
+        @list = LinkedList.new
+         self.append(data)
     end
+#  ITERATION III
+    # def initialize
+    #     @list = LinkedList.new #Created a new LinkedList that is stored in BeatBox
+    # end
 
-    def append(data)
-        data_split = (data.to_s).split
-        data_split.each do |data|
-            @list.append(data)
+# ITERATION V
+    def append(data) # mississippi
+        starter_list = "tee dee deep bop boop la na".split
+        verification = ""
+        starter_list.each do |accepted_data| # look at each word in the array
+            verify = (data.eql? accepted_data).to_s + " " # missisippi != look => false
+            verification << verify #stores "false "
+        end
+        if verification.include? "true"
+             data_split = (data.to_s).split
+             data_split.each do |data|
+                @list.append(data)
+             
+        end
         end
     end
+#  ITERATION III
+    # def append(data)
+    #     data_split = (data.to_s).split
+    #     data_split.each do |data|
+    #         @list.append(data)
+    #     end
+    # end
 
     def count
         list.count
@@ -29,4 +51,4 @@ end
 
 # bb = BeatBox.new
 # bb.append("deep doo ditt")
-# binding.pry
+binding.pry
