bed385db25f69aad1e9d7ffe96d5f6dac050f338
Anthea Yur <acyur6@gmail.com>
2023-08-29 16:50:19 -0500

    Refactor and include Iteration III for spec and code

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -55,10 +55,32 @@ class BeatBox
     def reset_voice
         @voice = "Boing"
     end
-    
+end 
 
-end
+## ITERATION III
 
-# bb = BeatBox.new
-# bb.append("deep doo ditt")
-#   binding.pry
+# class BeatBox
+
+#     attr_reader :list
+
+#     def initialize
+#         @list = LinkedList.new #Created a new LinkedList that is stored in BeatBox
+#     end
+
+#     def append(data)
+#         data_split = (data.to_s).split
+#         data_split.each do |data|
+#             @list.append(data)
+#         end
+#     end
+
+#     def count
+#         list.count
+#     end
+
+#     def play
+#         beats = list.to_string
+#         `say -r 500 -v Boing #{beats}` 
+#     end
+
+# end
