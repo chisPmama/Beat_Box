1b6ea6744976b0354b562cc4c7c62efac1cb2aa8
Anthea Yur <acyur6@gmail.com>
2023-08-29 13:52:18 -0500

    Spec verifies that new Beat Box works, Solo Project Complete

--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -4,46 +4,77 @@ require "./lib/beat_box"
 require "rspec"
 
 describe BeatBox do
-        it 'can instantiate LinkedList list' do
+    describe "#Iteration3" do
+        xit 'can instantiate LinkedList list' do
             bb = BeatBox.new
             expect(bb).to be_a BeatBox
         end
 
-        it 'created a list thats head is empty' do
+        xit 'created a list thats head is empty' do
             bb = BeatBox.new
             expect(bb.list.head). to be_nil
         end
 
-        it 'can append a new string or a node of multiple words' do
+        xit 'can append a new string or a node of multiple words' do
             bb = BeatBox.new
             bb.append("deep doo ditt")
             expect(bb.list.to_string).to eq("deep doo ditt")
         end
 
-        it 'appended and set the head to only the first part of the multiple word string' do
+        xit 'appended and set the head to only the first part of the multiple word string' do
             bb = BeatBox.new
             bb.append("deep doo ditt")
             expect(bb.list.head.data).to eq("deep")
         end
 
-        it 'checks if the head is attached to the next node' do
+        xit 'checks if the head is attached to the next node' do
             bb = BeatBox.new
             bb.append("deep doo ditt")
             expect(bb.list.head.next_node.data).to eq("doo")
         end
 
-        it 'can count the nodes and separate them properly' do
+        xit 'can count the nodes and separate them properly' do
             bb = BeatBox.new
             bb.append("deep doo ditt")
             bb.append("woo hoo shu")
             expect(bb.list.count).to eq(6)
         end
 
-        it 'can count when only calling bb.count' do
+        xit 'can count when only calling bb.count' do
             bb = BeatBox.new
             bb.append("deep doo ditt")
             bb.append("woo hoo shu")
             expect(bb.count).to eq(6)
         end
+    end
 
+end
+
+describe "#Iteration4" do
+    it 'can filter and insert multiple word string and properly append' do
+        bb = BeatBox.new("deep dop dop deep love")
+        expect(bb.all).to eq("deep dop dop deep")
+    end
+
+    it "can change the rate" do
+        bb = BeatBox.new("deep dop dop deep love")
+        bb.rate = 100
+        expect(bb.rate).to eq(100)
+    end
+
+    it "can change the voice" do
+        bb = BeatBox.new("deep dop dop deep love")
+        bb.voice = "Daniel"
+        expect(bb.voice).to eq("Daniel")
+    end
+
+    it "can reset voice and rate" do
+        bb = BeatBox.new("deep dop dop deep love")
+        bb.voice = "Daniel"
+        bb.rate = 100
+        bb.reset_rate
+        bb.reset_voice
+        expect(bb.rate).to eq(500)
+        expect(bb.voice).to eq("Boing")
+    end
 end
